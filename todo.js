88eefff1dc73d236e0ff88634d1d54109b34a0bc
Aritra1996 <arakshit.engineer@gmail.com>
2023-03-03 04:48:36 +0530

    Styling and Add Task Functionality added

--- todo.js
+++ todo.js
@@ -5,25 +5,43 @@ const tasksCounter = document.getElementById('tasks-counter');
 
 console.log('Working');
 
+function increaseCounter() {
+    const count = parseInt(tasksCounter.textContent);
+    tasksCounter.innerText = count+1;
+}
+
+function decreaseCounter() {
+    const count = parseInt(tasksCounter.textContent);
+    tasksCounter.innerText = count-1;
+}
+
 function renderList () {}
 
 function markTaskAsComplete (taskId) {}
 
 function deleteTask (taskId) {}
 
-function addTask (task) {}
+function addTask (task) {
+    let li = document.createElement("li");
+    li.innerText = task;
+    taskList.appendChild(li);
+    increaseCounter();
+}
 
-function showNotification(text) {}
+function showNotification(text) {
+    alert(text);
+}
 
 function handleInputKeypress (event) {
     if (event.key === 'Enter') {
         const text = event.target.value;
-
-        if ( !text){
+        console.log(text);
+        if (!text){
             showNotification('Task test cannot be empty');
-            return ;
+        } else {
+            addTask(text);
         }
     }
 }
 
-addTaskUnput.addEventListener('keyup', handleInputKeypress );
+addTaskInput.addEventListener('keyup', handleInputKeypress );
